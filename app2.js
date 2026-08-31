deaf944c5755168ef6667725364a284add4020a1
tanishkaverma1204 <tanishkaverma1204@gmail.com>
2026-08-31 11:13:17 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,16 @@
+const EventEmitter = require("events");
+
+class MyEmitter extends EventEmitter {}
+
+const event = new MyEmitter();
+
+event.on("greet", (name) => {
+    console.log(`hello ${name}`);
+});
+
+event.on("exit", () => {
+    console.log("exits mycustom event emitter...");
+});
+
+event.emit("greet", "cse36");
+event.emit("exit");
